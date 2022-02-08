663db429c50d0e7b7ddaff8ab0fb9f3a685cd9bd
Bruno Costa <brnocstaslva@gmail.com>
2022-02-08 18:15:51 -0300

    Implementação do método POST para a rota /users/:id

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { v4 as uuid } from 'uuid'
 
 const app = express()
 
@@ -12,6 +13,16 @@ const users: Users[] = []
 
 app.get('/users', (esquest, response) => {
     return response.json({ users })
+})
+
+app.post('/users/:id', (request, response) => {
+    const { name, email } = request.body
+
+    const user = { id: uuid(), name, email }
+
+    users.push(user)
+    
+    return response.json(user)
 })
 
 app.listen('3333', () => {
