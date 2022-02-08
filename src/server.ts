35d2adb309f594e4217089ef2aa944c172c2233d
Bruno Costa <brnocstaslva@gmail.com>
2022-02-08 18:02:47 -0300

    Adicionando uma ilista de usuários para representar um banco de dados

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,14 @@ import express from 'express'
 
 const app = express()
 
+interface Users {
+    id: string
+    name: string
+    email: string
+}
+
+const users: Users[] = []
+
 app.listen('3333', () => {
     console.log('Backend started!')
 })
